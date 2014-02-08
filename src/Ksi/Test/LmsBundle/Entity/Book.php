478e1d34fabfe8f31d968222d09912fcecbdf32c
kapilsharma <kapil@kapilsharma.info>
2014-02-08 13:54:18 +0530

    entity

---
+++ src/Ksi/Test/LmsBundle/Entity/Book.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Ksi\Test\LmsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Book
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Ksi\Test\LmsBundle\Entity\BookRepository")
+ */
+class Book
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="isbn", type="string", length=20)
+     */
+    private $isbn;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="publishDate", type="date")
+     */
+    private $publishDate;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="price", type="float")
+     */
+    private $price;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Book
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set isbn
+     *
+     * @param string $isbn
+     * @return Book
+     */
+    public function setIsbn($isbn)
+    {
+        $this->isbn = $isbn;
+
+        return $this;
+    }
+
+    /**
+     * Get isbn
+     *
+     * @return string 
+     */
+    public function getIsbn()
+    {
+        return $this->isbn;
+    }
+
+    /**
+     * Set publishDate
+     *
+     * @param \DateTime $publishDate
+     * @return Book
+     */
+    public function setPublishDate($publishDate)
+    {
+        $this->publishDate = $publishDate;
+
+        return $this;
+    }
+
+    /**
+     * Get publishDate
+     *
+     * @return \DateTime 
+     */
+    public function getPublishDate()
+    {
+        return $this->publishDate;
+    }
+
+    /**
+     * Set price
+     *
+     * @param float $price
+     * @return Book
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return float 
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+}
---
+++ src/Ksi/Test/LmsBundle/Entity/Publisher.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace Ksi\Test\LmsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrint\Common\Collections\ArrayCollection;
+
+/**
+ * Publisher
+ *
+ * @ORM\Table(name="publisher")
+ * @ORM\Entity(repositoryClass="Ksi\Test\LmsBundle\Entity\PublisherRepository")
+ */
+class Publisher
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    
+    /**
+     * @ORM\OneToMany(targetEntity="Book", mappedBy="publisher_id")
+     */
+    private $books;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="address", type="text")
+     */
+    private $address;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="country", type="string", length=15)
+     */
+    private $country;
+    
+    /**
+     * Constructor. Ensuring Books is an ArrayCollection
+     */
+    public function _construct()
+    {
+        $this->books = new ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    /**
+     * Get Books
+     * 
+     * @return Doctrine\Common\Collections\ArrayCollection Books
+     */
+    public function getBooks()
+    {
+        return $this->books;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Publisher
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set address
+     *
+     * @param string $address
+     * @return Publisher
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    /**
+     * Get address
+     *
+     * @return string 
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * Set country
+     *
+     * @param string $country
+     * @return Publisher
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    /**
+     * Get country
+     *
+     * @return string 
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+}
